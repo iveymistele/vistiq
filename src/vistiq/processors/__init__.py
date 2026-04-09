5fed67c4a02afcd25a2bb6fbd82ae0be4e2d96df
Ivey Mistele <iveymistele@gmail.com>
2026-04-09 01:23:22 -0400

    Fix typo

--- src/vistiq/processors/__init__.py
+++ src/vistiq/processors/__init__.py
@@ -1,4 +1,4 @@
 from vistiq.processors.base import BaseProcessor
 from vistiq.processors.pipeline import ProcessorPipeline
 
-__all__ = ["BaseProcessor", "ProcessorPipeline", debug: bool = False]
+__all__ = ["BaseProcessor", "ProcessorPipeline"]
